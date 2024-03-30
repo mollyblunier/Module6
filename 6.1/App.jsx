9cab14ebbf130ae5709a324aba1fc7ecb28b9101
mollyblunier <153697061+mollyblunier@users.noreply.github.com>
2024-03-30 10:49:27 -0400

    Add files via upload

---
+++ 6.1/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Greeting from './components/Greeting'
+
+function App() {
+    return (
+        <>
+        <Greeting name="Spider Man">
+            <div>With great power comes great responsibility</div>
+        </Greeting>
+        </>
+    )
+}
